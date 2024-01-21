c02f729060537015ddef4f494781d9af7dc92a23
Andrey <thekanas@yandex.ru>
2024-01-21 12:15:10 +0300

    siple hashmap
    two

---
+++ src/main/java/org/example/simplehashmap/SimpleHashMapImpl.java
@@ -0,0 +1,149 @@
+package org.example.simplehashmap;
+
+import java.util.LinkedList;
+
+/**
+ * Класс представляет упрощенную реализацию HashMap
+ * @param <K> ключ по которому храниться соответствующее значение
+ * @param <V> значение соответствующее заданному ключу
+ */
+public class SimpleHashMapImpl<K, V> implements SimpleHashMap<K, V> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private LinkedList<Entry<K, V>>[] buckets;
+    private int size = 0;
+    private int capacity;
+    private final float loadFactor;
+
+    /**
+     * Конструктор без параметров,
+     * задающий дефолтные значения:
+     * capacity = 16
+     * loadFactor = 0.75
+     */
+    public SimpleHashMapImpl() {
+        this.capacity = DEFAULT_CAPACITY;
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+        this.buckets = new LinkedList[capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    /**
+     * Конструктор с параметрами
+     * @param capacity емкость массива баккетов
+     * @param loadFactor задаёт отношение кол-ва объектов, хранящихся в SimpleHashMap,
+     *                  к емкости (capacity), при превыщении которого емкость увеличивается вдвое (х2)
+     */
+    public SimpleHashMapImpl(int capacity, float loadFactor) {
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+        this.buckets = new LinkedList[capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        int index = hashFunction(key, capacity);
+        LinkedList<Entry<K, V>> bucket = buckets[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void remove(K key) {
+        int index = hashFunction(key, capacity);
+        LinkedList<Entry<K, V>> bucket = buckets[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.getKey().equals(key)) {
+                bucket.remove(entry);
+                size--;
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+        if ((float) (size + 1) / capacity > loadFactor) {
+            resize();
+        }
+
+        int index = hashFunction(key, capacity);
+        LinkedList<Entry<K, V>> bucket = buckets[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.getKey().equals(key)) {
+                entry.setValue(value);
+                return;
+            }
+        }
+
+        bucket.add(new Entry<>(key, value));
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void resize() {
+        int newCapacity = capacity * 2;
+        LinkedList<Entry<K, V>>[] newBuckets = new LinkedList[newCapacity];
+
+        for (int i = 0; i < newCapacity; i++) {
+            newBuckets[i] = new LinkedList<>();
+        }
+
+        for (LinkedList<Entry<K, V>> bucket : buckets) {
+            for (Entry<K, V> entry : bucket) {
+                int newIndex = hashFunction(entry.getKey(), newCapacity);
+                newBuckets[newIndex].add(entry);
+            }
+        }
+
+        this.buckets = newBuckets;
+        this.capacity = newCapacity;
+    }
+
+    private int hashFunction(K key, int capacity) {
+        return key.hashCode() % capacity;
+    }
+
+    private static class Entry<K, V> {
+        private final K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/org/example/simplehashmap/SimpleHashMapTest.java
@@ -0,0 +1,124 @@
+package org.example.simplehashmap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+class SimpleHashMapTest {
+
+    @Test
+    void getTest() {
+        // given
+        SimpleHashMap<Integer, String> testHashMap = new SimpleHashMapImpl<>();
+        String expected1 = "test1";
+        String expected2 = "test2";
+        String expected3 = "test3";
+        String expected4 = "test2+";
+
+        testHashMap.put(1, expected1);
+        testHashMap.put(2, expected2);
+        testHashMap.put(3, expected3);
+
+        // when
+        String actual1 = testHashMap.get(1);
+        String actual2 = testHashMap.get(2);
+        String actual3 = testHashMap.get(3);
+
+        // then
+        assertEquals(actual1, expected1);
+        assertEquals(actual2, expected2);
+        assertEquals(actual3, expected3);
+
+        // when
+        testHashMap.put(2, expected4);
+        String actual4 = testHashMap.get(2);
+        String actual5 = testHashMap.get(4);
+
+        // then
+        assertEquals(actual4, expected4);
+        assertNull(actual5);
+
+
+    }
+
+    @Test
+    void removeTest() {
+        // given
+        SimpleHashMap<Integer, String> testHashMap = new SimpleHashMapImpl<>();
+
+        testHashMap.put(1, "test1");
+        testHashMap.put(2, "test2");
+        testHashMap.put(3, "test3");
+
+        // when
+        testHashMap.remove(1);
+
+        // then
+        assertEquals(testHashMap.size(), 2);
+
+        // when
+        testHashMap.remove(2);
+
+        // then
+        assertEquals(testHashMap.size(), 1);
+
+        // when
+        testHashMap.remove(2);
+
+        // then
+        assertEquals(testHashMap.size(), 1);
+
+    }
+
+    @Test
+    void putTest() {
+        // given
+        SimpleHashMap<Integer, String> testHashMap = new SimpleHashMapImpl<>(2, 0.75f);
+        for (int i = 1; i < 100; i++) {
+            testHashMap.put(i, "test" + i);
+
+        }
+
+        // when
+        String actual1 = testHashMap.get(1);
+
+        // then
+        assertEquals(actual1, "test1");
+
+        // when
+        String actual2 = testHashMap.get(99);
+
+        // then
+        assertEquals(actual2, "test99");
+
+    }
+
+    @Test
+    void sizeTest() {
+        // given
+        SimpleHashMap<Integer, String> testHashMap = new SimpleHashMapImpl<>(2, 0.75f);
+
+        testHashMap.put(1, "test1");
+        testHashMap.put(2, "test2");
+        testHashMap.put(3, "test3");
+
+        // when
+        // then
+        assertEquals(testHashMap.size(), 3);
+
+        // when
+        testHashMap.remove(1);
+
+        // then
+        assertEquals(testHashMap.size(), 2);
+
+        // when
+        testHashMap.put(2, "test2+");
+
+        // then
+        assertEquals(testHashMap.size(), 2);
+    }
+
+}
